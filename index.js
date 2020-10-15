bf5a1163404518b69daab948eedaa21d2e891c41
Nayeli C <nayelicruz12@gmail.com>
2020-10-15 15:16:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function currentLine(line) {
   if (line.length === 0) {
     return 'The line is currently empty.'
   } else {
-    return `The line is currently: `
+    for (let i = 0; i < line.length; i++) {
+    return `The line is currently: ${line.length}. ${line[i]}`  
+    }
   }
 }
